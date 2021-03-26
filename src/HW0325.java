ad6226d81da6024ae9d918cd17707d53753e3aec
Oleg <Oleg-BLR@yandex.ru>
2021-03-26 13:30:10 +0300

    Домашнее задание 03-25

--- src/HW0325.java
+++ src/HW0325.java
@@ -8,7 +8,7 @@ public class HW0325 {
         arr [1] = -3;
         arr [2] = -4;
         int s = 0;
-        for (int i = 0; i < arr.length; i++){ //'for' loop replaceable with enhanced 'for'
+        for (int i = 0; i < arr.length; i++){ // ! 'for' loop replaceable with enhanced 'for'
             if (arr [i] > 0) s += 1;
         }
         System.out.println(s);
