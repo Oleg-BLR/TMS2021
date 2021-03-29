437d9928bd523a13e16c66cda5bff571f856028b
Oleg <Oleg-BLR@yandex.ru>
2021-03-29 15:10:42 +0300

    Домашнее задание 25 марта

--- src/Task1.java
+++ src/Task1.java
@@ -1,8 +1,8 @@
 public class Task1 {
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 0; i < args.length; i++)
-            System.out.println(args[i]);
+        for (int i = 0; i < args.length; i++) //вывод введенных значений в консоль
+            System.out.println(args[i]);      // Подскажите, как сделать через цикл FOREACH
         for (int i = 0; i < args.length; i++){
             int n = Integer.parseInt(args[i]);
             if (n > 0){
