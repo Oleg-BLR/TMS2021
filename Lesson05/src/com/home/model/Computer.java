6ebf8e6c1fcb1b74bf8c3c0757148a2f1e1b9b49
Oleg <Oleg-BLR@yandex.ru>
2021-04-12 13:38:13 +0300

    Добрый день, Павел! Создал класс Computer, где прописал, как понимаю поля этого класса, а также методы для включения и выключения. Создал класс Main, но пока трудно понять, что надо там прописывать, чтобы все вместе заработало.

---
+++ Lesson05/src/com/home/model/Computer.java
@@ -0,0 +1,48 @@
+package com.home.model;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
+    String processor;
+    int ram;
+    int hdd;
+    int operationLifeCicles;
+    boolean workPc = true;
+
+
+    void description() {
+        System.out.println("Процессор - "processor + ", Оперативная память - " + ram + ", Жесткий диск - " + hdd + ", Ресурс - " + operationLifeCicles);
+    }
+
+    private void on() {
+        boolean workPc = true;
+        System.out.println("Внимание! Введите 0 или 1");
+        Scanner scan = new Scanner(System.in);
+        int myZeroOrOne = scan.nextInt();
+        Random randZeroOrOne = new Random();
+        while (operationLifeCicles < this.operationLifeCicles) {
+            while (this.workPc) {
+                if (myZeroOrOne == randZeroOrOne) {
+                    off();
+                } else {
+                    System.out.println("Компьютер сгорел!");
+                    this.workPc = false;
+                }
+            }
+        }
+    }
+
+    private void off() {
+        System.out.println("Внимание! Введите 0 или 1");
+        Scanner scan = new Scanner(System.in);
+        int myZeroOrOne = scan.nextInt();
+        Random randZeroOrOne = new Random();
+        if (myZeroOrOne == randZeroOrOne) {
+            on();
+        } else {
+            System.out.println("Компьютер сгорел!");
+            this.workPc = false;
+        }
+    }
+}
